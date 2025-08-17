dfe5f1893da43b1adc5811731790f952be2bef01
Darshna-Choudhary <darshnachoudhary1823@gmail.com>
2025-08-17 12:01:46 +0530

    Q4

---
+++ Array/Q4_sort_array_contaninig_0_1_2_without_using_sorting_algo.py
@@ -0,0 +1,20 @@
+# Input: arr = [0, 1, 2, 0, 1, 2]
+# Output: [0, 0, 1, 1, 2, 2]
+def sort012(arr):
+	n = len(arr)
+    low = 0
+    mid = 0
+    high = n-1
+    while mid <= high:
+		if arr[mid] == 0:
+			arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 2:
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+        else:
+            mid += 1
+	return arr
+
+print(sort012(arr)
