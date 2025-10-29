34d2d78d5a097fbbfdae451abdb996bd64834a13
Darshna-Choudhary <darshnachoudhary1823@gmail.com>
2025-10-29 17:23:06 +0530

    Q52
    Refactor countAndSay function by adding encode and decode helper functions for better readability and structure.

--- Strings/Q52_count_and_say_problem.py
+++ Strings/Q52_count_and_say_problem.py
@@ -1,22 +1,31 @@
 # Input: n = 4
 # Output: "1211"
 
+def encode(s):
+    freq = []
+    count = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            count += 1
+        else:
+            freq.append([s[i-1], count])
+            count = 1
+    freq.append([s[-1], count])
+    return freq
+    
+def decode(dct):
+    s = ""
+    for ch, cnt in dct:
+        s += str(cnt) + ch
+    return s
+
 def countAndSay(n):
     if n == 1:
         return str(n)
-
     ans = "1"
-    for _ in range(n-1):
-        curr = ""
-        count = 1
-        for i in range(1, len(ans)):
-            if ans[i] == ans[i-1]:
-                count += 1
-            else:
-                curr += str(count) + str(ans[i-1])
-                count = 1
-        curr += str(count) + str(ans[-1])
-        ans = curr
+    for i in range(1, n):
+        f = encode(ans)
+        ans = decode(f)
     return ans
 
-print(countAndSay(n))
+print(countAndSay(4))
