b794b264c2fbaf4f0000cef3549793a028b3182a
Darshna-Choudhary <darshnachoudhary1823@gmail.com>
2026-06-01 09:55:36 +0530

    Q1

---
+++ Hackerrank/Q1_min_max_sum.py
@@ -0,0 +1,9 @@
+def miniMaxSum(arr):
+    min_val = float("inf")
+    max_val = float("-inf")
+    sum = 0
+    for i in arr:
+        min_val = min(min_val,i)
+        max_val = max(max_val, i)
+        sum += i
+    print((sum-max_val), (sum-min_val))
